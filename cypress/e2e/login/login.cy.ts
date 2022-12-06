418fb8ebc8ff60656e822b39061d706ea916914a
sidimar.coelho <sidimar.coelho@nexaas.com>
2022-12-06 00:47:36 -0300

    Login

--- cypress/e2e/login/login.cy.ts
+++ cypress/e2e/login/login.cy.ts
@@ -1,22 +1,27 @@
+const fakerdate = require('faker');
+
 describe('Dado que estou na tela criação de usuário', () => {
 
-  context('Quando preencho todos os campos e finalizo a criação', () => {
-        it('Então ao preencher ', () => {
-            cy.visit('/').then(()=>{
-                cy.enter('#oneid-iframe').then(getBory =>{
+    context('Quando preencho todos os campos e finalizo a criação', () => {
+        it('Então ao preencher ', {includeShadowDom: true},() => {
+            cy.visit('/').then(() => {
+                cy.enter('#oneid-iframe').then(getBory => {
                     getBory().find("#BtnCreateAccount").click()
                     var faker = require('faker-br');
-                    getBory().get('[data-testid="checkbox-show-pw-wrapper"]').then(()=>{
-                        getBory().find('#checkbox-show-pw').click()
-                    })
-
                     getBory().find('#InputFirstName').type(faker.name.firstName())
                     getBory().find('#InputLastName').type(faker.name.lastName())
                     getBory().find('#InputEmail').type(faker.internet.email())
                     getBory().find('#password-new').type(faker.internet.password())
-            })
+                    getBory().find('input[data-testid^="LOCATION_"]').click({force: true})
+                    getBory().find('input[data-testid^="BU_*_LOCATION"]').click({force: true})
+                    // getBory().find('[data-testid="InputDOB"]').type(`${fakerdate.datatype.number({min:10,max:29}) }`)
+                    getBory().find('[data-testid="InputDOB"]').clear({force: true,}).type('11112010')
 
-                // getBory().find('#InputDOB').type("1612984")
+                    getBory().find('button#BtnSubmit').click()
+
+
+                })
+
             })
         })
     })
